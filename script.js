cf130d842dee4cec7d18a7c1e6e5102976d8b458
Nicholas Lim <nicholas_lim@bbsshack.club>
2026-06-23 11:05:38 +0800

    Slight changes to the panel

--- script.js
+++ script.js
@@ -7,7 +7,34 @@ function bootScreen() {
 setTimeout(() => {bootScreen()}, 2000);
 
 function time() {
-    document.getElementById("time").textContent = new Date().toLocaleString();
+    date = new Date();
+    month = date.getMonth() + 1;
+    if (month == 1) {
+        monthName = "January";
+    } else if (month == 2) {
+        monthName = "February";
+    } else if (month == 3) {
+        monthName = "March";
+    } else if (month == 4) {
+        monthName = "April";
+    } else if (month == 5) {
+        monthName = "May";
+    } else if (month == 6) {
+        monthName = "June";
+    } else if (month == 7) {
+        monthName = "July";
+    } else if (month == 8) {
+        monthName = "August";
+    } else if (month == 9) {
+        monthName = "September";
+    } else if (month == 10) {
+        monthName = "October";
+    } else if (month == 11) {
+        monthName = "November";
+    } else if (month == 9) {
+        monthName = "December";
+    }
+    document.getElementById("time").innerHTML = `${date.getDate()} ${monthName} ${date.getFullYear()}, ${date.toLocaleTimeString()}`;
 };
 
 setInterval(time, 500);
